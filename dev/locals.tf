8aa16985ced7d205ed5ecb758ca324c0f170745b
victor.vogt <victor.vogt@chargemap.com>
2026-04-19 09:22:33 +0200

    instance as DEV1-S

--- dev/locals.tf
+++ dev/locals.tf
@@ -2,7 +2,7 @@ locals {
   region        = "fr-par"
   zone          = "fr-par-1"
   environment   = "dev"
-  instance_type = "STARDUST1-S"
+  instance_type = "DEV1-S"
   image         = "debian_trixie"
 
   allowed_ip_range = "88.138.79.90/32"
